05c5d94f76c13a055ceb4a77afc740e03ddcc4f9
Sepehr <sepehr.gouran@gmail.com>
2017-07-24 15:31:09 +0430

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Sepehr on 7/24/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[][] table = new int[5][5];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(table[i][j] + "\t");
+            }
+            System.out.println("");
+        }
+
+    }
+}
