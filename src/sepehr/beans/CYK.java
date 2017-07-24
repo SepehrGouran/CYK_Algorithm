6ec935b71655811c406a41dff4656ce146fe9585
Sepehr <sepehr.gouran@gmail.com>
2017-07-24 15:38:14 +0430

    CYK class

--- src/sepehr/beans/CYK.java
+++ src/sepehr/beans/CYK.java
@@ -4,4 +4,18 @@ package sepehr.beans;
  * Created by Sepehr on 7/24/2017.
  */
 public class CYK {
+
+    private String grammar;
+
+    public CYK(String grammar) {
+        this.grammar = grammar;
+    }
+
+    public String getGrammar() {
+        return grammar;
+    }
+
+    public void setGrammar(String grammar) {
+        this.grammar = grammar;
+    }
 }
